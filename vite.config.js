4d540c64b68ba94634564433dee26b21610750c8
mohithworks <mohithworks2002@gmail.com>
2023-03-07 13:14:43 +0530

    Fixed bugs

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,12 @@ export default defineConfig({
   resolve: {
     alias: [{ find: "@", replacement: "/src" }],
   },
-  define: {
-    // Some libraries use the global object, even though it doesn't exist in the browser.
-    // Alternatively, we could add `<script>window.global = window;</script>` to index.html.
-    // https://github.com/vitejs/vite/discussions/5912
-    global: {},
-  },
+  define: {global: 'window'},
+  optimizeDeps: {
+    esbuildOptions: {
+      define: {
+        global: 'globalThis'
+      }
+    }
+  }
 });
